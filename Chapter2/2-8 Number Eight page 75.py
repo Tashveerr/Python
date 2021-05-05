ce326961bbc5b28530ebca9b5ff76318b185f7e1
Tashveerr <52349565+Tashveerr@users.noreply.github.com>
2021-05-05 12:42:23 +0200

    Delete Chapter2 directory

--- Chapter2/2-8 Number Eight page 75.py
+++
@@ -1,8 +0,0 @@
-print(5+3)
-
-print(2*4)
-
-print(16/2)
-
-print(10-2)
-
--- Chapter2/2-9 Favourite Number page 75.py
+++
@@ -1,3 +0,0 @@
-favourite_number = "7"
-message =f'{favourite_number}"Is my favourite number (favourite_number)"'
-print (message)
